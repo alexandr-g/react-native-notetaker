d9e5926762b3f67227397331ef62fdf90b5228db
Alexandr <alexgrischuk@gmail.com>
2016-12-26 19:14:03 +0100

    Add view Profile, Repos, Notes buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -23,12 +23,61 @@ const styles = StyleSheet.create({
 })
 
 export default class Dashboard extends Component {
+
+  makeBackground(btn) {
+    let obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+
+
+    if ( btn === 0 ) {
+      obj.backgroundColor = '#48BBEC'
+    } else if ( btn === 1 ) {
+      obj.backgroundColor = '#E77AAE'
+    } else {
+      obj.backgroundColor = '#758BF4'
+    }
+
+    return obj
+  }
+
+  goToProfile() {
+    console.log('Going to Profile Page')
+  }
+
+  goToRepos() {
+    console.log('Going to Repos')
+  }
+
+  goToNotes() {
+    console.log('Going to Notes')
+  }
+
   render() {
     return(
       <View style={styles.container}>
         <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
-        <Text> Dashboard </Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Notes </Text>
+        </TouchableHighlight>
       </View>
     )
   }
