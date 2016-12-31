0378eddf0339584eac542d7d8e4bf84d9e863cf0
Alexandr <alexgrischuk@gmail.com>
2016-12-31 17:53:15 +0100

    Extend api to use firebase for notes

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,6 +10,21 @@ let api = {
     let url = `https://api.github.com/users/${username}/repos`
 
     return fetch(url).then((res) => res.json())
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim()
+    let url = `https://react-notetaker-a4f92.firebaseio.com/${username}.json`
+
+    return fetch(url).then((res) => res.json())
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim()
+    let url = `https://react-notetaker-a4f92.firebaseio.com/${username}.json`
+
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json())
   }
 }
 
